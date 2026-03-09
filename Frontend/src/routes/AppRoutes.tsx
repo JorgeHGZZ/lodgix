6b8552c56cf25299373ec0d2679ca524957850dc
FannyM12 <fannymorales214@gmail.com>
2026-03-09 10:12:33 -0600

    Update AppRoutes.tsx

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -11,6 +11,7 @@ import Clients from "../pages/dashboard/Clients";
 import Maintenance from "../pages/dashboard/Maintenance";
 import Cleaning from "../pages/dashboard/CleanService";
 import Reports from "../pages/dashboard/Reports";
+import DetailRoom from "../pages/dashboard/DetailRoom";
 
 const AppRouter = () => {
     return (
@@ -19,6 +20,7 @@ const AppRouter = () => {
                 <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<SignUp />} />
+               
 
                 <Route path="/dashboard" element={<Dashboard />}>
                     <Route index element={<Rooms />} />
@@ -28,6 +30,7 @@ const AppRouter = () => {
                     <Route path="mantenimiento" element={<Maintenance />} />
                     <Route path="limpieza" element={<Cleaning />} />
                     <Route path="reportes" element={<Reports />} />
+                    <Route path="detailroom" element={<DetailRoom  />} />
                 </Route>
             </Routes>
         </BrowserRouter>
