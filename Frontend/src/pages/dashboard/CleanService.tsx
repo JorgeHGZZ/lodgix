10309f46e41414d61c4c965f9efee1ffedaf5672
FannyM12 <fannymorales214@gmail.com>
2026-03-09 09:33:57 -0600

    Update CleanService.tsx

--- Frontend/src/pages/dashboard/CleanService.tsx
+++ Frontend/src/pages/dashboard/CleanService.tsx
@@ -1,9 +1,63 @@
+import "../../styles/clean-service.css";
+
 function CleanService(){
     return (
+        <>
         <div>
-            <h1>CleanService del Dashboard</h1>
-            <p>Si ves esto, el Outlet está funcionando.</p>
+            <h1>Servicio de limpieza</h1>
+            {/* <p>Si ves esto, el Outlet está funcionando.</p> */}
         </div>
+
+         <div className="cleaning-container">
+
+      <div className="cleaning-header">
+
+        <div className="filters">
+          <input type="text" placeholder="Buscar habitación..." />
+
+          <select>
+            <option>Estado</option>
+            <option>Pendiente</option>
+            <option>En limpieza</option>
+            <option>Limpia</option>
+          </select>
+
+           <button className="btn-add">Buscar</button>
+
+          <button className="btn-add">+ Asignar limpieza</button>
+        </div>
+      </div>
+
+      <table className="cleaning-table">
+        <thead>
+          <tr>
+            <th>Habitación</th>
+            <th>Estado</th>
+            <th>Tipo</th>
+            <th>Empleado</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>301</td>
+            <td className="status-cleaning">En limpieza</td>
+            <td>Estándar</td>
+            <td>María</td>
+          </tr>
+
+          <tr>
+            <td>204</td>
+            <td className="status-done">Limpia</td>
+            <td>Profunda</td>
+            <td>Ana</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+        
+        </>
     )
 }
 
