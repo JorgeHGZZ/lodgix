de717718cec8e38fe7efaa81de7990bca72be50f
FannyM12 <fannymorales214@gmail.com>
2026-04-26 15:41:55 -0600

    Update Searchbar.tsx

--- Frontend/src/components/layout/Searchbar.tsx
+++ Frontend/src/components/layout/Searchbar.tsx
@@ -4,10 +4,13 @@ import Button from "../ui/Button";
 
 function Searchbar() {
     return(
+        <div className="search-container">
         <div className="searchbar">
-            <Input type="text" placeholder="Search..."></Input>
-            <br></br>
+            <Input type="text" placeholder="Buscar..."></Input>
+        </div>
+        <div>
             <Button  classname="search-btn" titulo="Buscar" />
+        </div>
         </div>
     )
 }
