ea6cc65428982c0f7246dc8728fd8fdbe05d307a
FannyM12 <fannymorales214@gmail.com>
2026-04-26 01:04:32 -0600

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from "./components/layout/Navbar"
 import Button from "./components/ui/Button"
 import "./styles/App.css"
 import dashboardimg from "./assets/images/dashboard.png"
-
 function App() {
 
   return (
