67ad103c7c7b43c4ab0985ad32b139c167b36d74
Jorge Gonzalez <humberto.gonzalezmrn@uanl.edu.mx>
2026-05-23 17:16:37 -0600

    Update Landing.tsx

--- Frontend/src/pages/Landing.tsx
+++ Frontend/src/pages/Landing.tsx
@@ -20,50 +20,38 @@ function Landing() {
                         Controla reservas, habitaciones, huéspedes y pagos desde un solo lugar.
                         Nuestro sistema centraliza toda la operación del hotel en una sola plataforma.
                     </p>
-
-                      <Button onClick={() => navigate('/login')} classname="btn" titulo="Empieza Ahora"></Button>
-                      
+                    <Button onClick={() => navigate('/login')} classname="btn" titulo="Empieza Ahora"></Button>
                 </div>
-
                 <img src={dashboardimg} alt="Dashboard" />
             </section>
 
             {/* FEATURES */}
             <section className="features">
-
                 <div className="feature-card">
                     <h3>Habitaciones</h3>
                     <p>Disponibilidad en tiempo real y control total de estados.</p>
                 </div>
-
                 <div className="feature-card">
                     <h3>Reservas</h3>
                     <p>Gestión rápida, sin errores ni overbooking.</p>
                 </div>
-
                 <div className="feature-card">
                     <h3>Pagos</h3>
                     <p>Control de ingresos y reportes claros.</p>
                 </div>
-
                 <div className="feature-card">
                     <h3>Reportes</h3>
                     <p>Analiza ocupación e ingresos fácilmente.</p>
                 </div>
-
                 <div className="feature-card">
                     <h3>Huéspedes</h3>
                     <p>Historial completo y datos organizados.</p>
                 </div>
-
             </section>
-
+            
             <section className="cta">
                 <h2>Empieza a gestionar tu hotel hoy</h2>
-               
-                     <Button onClick={() => navigate('/login')} classname="btn" titulo="Empieza Ahora"></Button>
-                
-                
+                <Button onClick={() => navigate('/login')} classname="btn" titulo="Empieza Ahora"></Button>
             </section>
 
             <footer>
